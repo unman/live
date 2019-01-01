703aadc0d9c81d0cf87ccc37084f52cc5899819b
unman <unman@thirdeyesecurity.org>
2019-01-01 14:50:53 +0000

    Correct debconf setting for ipw2x00 license acceptance

--- qubes-builder-debian/template_debian/firmware/02_install_groups_packages_installed.sh
+++ qubes-builder-debian/template_debian/firmware/02_install_groups_packages_installed.sh
@@ -7,6 +7,6 @@ source "${SCRIPTSDIR}/distribution.sh"
 #### '----------------------------------------------------------------------
 info ' Installing firmware'
 #### '----------------------------------------------------------------------
-chroot_cmd sh -c 'echo "firmware-ipw2x00/license/accepted select true" |debconf-set-selections'
+chroot_cmd sh -c 'echo "firmware-ipw2x00 firmware-ipw2x00/license/accepted select true" |debconf-set-selections'
 packages="atmel-firmware firmware-atheros firmware-brcm80211 firmware-ipw2x00 firmware-iwlwifi firmware-misc-nonfree firmware-realtek firmware-zd1211"
 aptInstall $packages
