e4de591e3c169a3b45e68209a357da687612a923
unman <unman@thirdeyesecurity.org>
2022-08-31 15:08:37 +0000

    Parrot build - update sources and signing key

--- qubes-builder-debian/template_debian/parrot/04_install_qubes_post.sh
+++ qubes-builder-debian/template_debian/parrot/04_install_qubes_post.sh
@@ -15,17 +15,21 @@ debug " Provisioning machine for Parrot installation
 # Create system mount points
 prepareChroot
 
-chroot_cmd apt-key add - < ${SCRIPTSDIR}/../keys/parrot.asc
+chroot_cmd apt-key add - < ${SCRIPTSDIR}/../keys/parrot-debian-archive-keyring.gpg 
 chroot_cmd apt-mark hold qubes-core-agent
 chroot_cmd apt-mark hold qubes-core-agent-networking
 chroot_cmd apt-mark hold qubes-gui-agent
 chroot_cmd apt-mark hold linux-image-amd64
 chroot_cmd apt-mark hold grub-pc
 
+
+sudo rm "${INSTALLDIR}/etc/apt/sources.list"
 sudo cat <<EOF > "${INSTALLDIR}/etc/apt/sources.list.d/parrot.list"  
 # ParrotOS repository  
-deb http://HTTPS///deb.parrotsec.org/parrot stable main contrib non-free
-#deb-src http://HTTPS///deb.parrotsec.org/parrot stable main contrib non-free
+deb http://HTTPS///deb.parrot.sh/parrot/ parrot main contrib non-free
+#deb-src http://HTTPS///deb.parrot.sh/parrot/ parrot main contrib non-free
+deb http://HTTPS///deb.parrot.sh/parrot/ rolling-security main contrib non-free
+#deb-src http://HTTPS///deb.parrot.sh/parrot/ rolling-security main contrib non-free
 EOF
 
 ## Ensure proxy handling is set
