c09c09c987ef710d2ea94049e634ccadefa33f49
unman <unman@thirdeyesecurity.org>
2024-05-30 03:02:11 +0000

    Parrot - update for 4.2 and qubesbuilderv2

--- qubes-builder-debian/template_debian/parrot/04_install_qubes_post.sh
+++ qubes-builder-debian/template_debian/parrot/04_install_qubes_post.sh
@@ -5,8 +5,8 @@ if [ "$VERBOSE" -ge 2 -o "$DEBUG" == "1" ]; then
     set -x
 fi
 
-source "${SCRIPTSDIR}/vars.sh"
-source "${SCRIPTSDIR}/distribution.sh"
+source "${TEMPLATE_CONTENT_DIR}/vars.sh"
+source "${TEMPLATE_CONTENT_DIR}/distribution.sh"
 
 ##### "=========================================================================
 debug " Provisioning machine for Parrot installation
@@ -15,7 +15,7 @@ debug " Provisioning machine for Parrot installation
 # Create system mount points
 prepareChroot
 
-chroot_cmd apt-key add - < ${SCRIPTSDIR}/../keys/parrot-debian-archive-keyring.gpg 
+chroot_cmd apt-key add - < ${TEMPLATE_CONTENT_DIR}/../keys/parrot-debian-archive-keyring.gpg 
 chroot_cmd apt-mark hold qubes-core-agent
 chroot_cmd apt-mark hold qubes-core-agent-networking
 chroot_cmd apt-mark hold qubes-gui-agent
@@ -23,25 +23,30 @@ chroot_cmd apt-mark hold linux-image-amd64
 chroot_cmd apt-mark hold grub-pc
 
 
-sudo rm "${INSTALLDIR}/etc/apt/sources.list"
-sudo cat <<EOF > "${INSTALLDIR}/etc/apt/sources.list.d/parrot.list"  
+sudo rm "${INSTALL_DIR}/etc/apt/sources.list"
+#sudo cat <<EOF > "${INSTALL_DIR}/etc/apt/sources.list.d/parrot.list"  
 # ParrotOS repository  
-deb http://HTTPS///deb.parrot.sh/parrot/ parrot main contrib non-free
-#deb-src http://HTTPS///deb.parrot.sh/parrot/ parrot main contrib non-free
-deb http://HTTPS///deb.parrot.sh/parrot/ rolling-security main contrib non-free
-#deb-src http://HTTPS///deb.parrot.sh/parrot/ rolling-security main contrib non-free
-EOF
+#deb http://HTTPS///deb.parrot.sh/parrot rolling main contrib non-free non-free-firmware
+#deb http://HTTPS///deb.parrot.sh/parrot lory main contrib non-free non-free-firmware
+#deb http://HTTPS///deb.parrot.sh/parrot lory-security main contrib non-free non-free-firmware
+#deb http://HTTPS///deb.parrot.sh/parrot lory-backports main contrib non-free non-free-firmware
+#deb-src http://HTTPS///deb.parrot.sh/parrot lory main contrib non-free non-free-firmware
+#deb-src http://HTTPS///deb.parrot.sh/parrot lory-security main contrib non-free non-free-firmware
+#deb-src http://HTTPS///deb.parrot.sh/parrot lory-backports main contrib non-free non-free-firmware
+#EOF
+sudo cp ${TEMPLATE_CONTENT_DIR}/parrot/parrot.list "${INSTALL_DIR}/etc/apt/sources.list.d/parrot.list"  
 
 ## Ensure proxy handling is set
 chroot_cmd sh -c 'sed -i s%https://%http://HTTPS///% /etc/apt/sources.list.d/* '
 
+read -p "Before Installing Parrot Packages "
 
 ##### "=========================================================================
 debug " Installing packages from ParrotOS
 ##### "=========================================================================
 aptDistUpgrade
 
-cat <<EOF >> "${INSTALLDIR}/etc/apt/preferences.d/1hold"  
+cat <<EOF >> "${INSTALL_DIR}/etc/apt/preferences.d/1hold"  
 
 Package: wireguard
 Pin: release *
@@ -56,20 +61,22 @@ Pin: release *
 Pin-Priority: -999
 EOF
 
+
 ## Parrot rewrites the sources lists
 ## Ensure proxy handling is still set
 chroot_cmd sh -c 'sed -i s%https://%http://HTTPS///% /etc/apt/sources.list.d/* '
 aptUpdate
 
-APT_GET_OPTIONS+=" --allow-downgrades"
-installPackages ${SCRIPTSDIR}/packages_parrot.list
+APT_GET_OPTIONS+=" --allow-downgrades=yes"
+installPackages ${TEMPLATE_CONTENT_DIR}/packages_parrot.list
+read -p "After Installing Parrot Packages "
 
 ##### "=========================================================================
 debug " ParrotOS
 ##### "=========================================================================
 
 # ==============================================================================
-# Kill all processes and umount all mounts within ${INSTALLDIR}, but not
-# ${INSTALLDIR} itself (extra '/' prevents ${INSTALLDIR} from being umounted)
+# Kill all processes and umount all mounts within ${INSTALL_DIR}, but not
+# ${INSTALL_DIR} itself (extra '/' prevents ${INSTALL_DIR} from being umounted)
 # ==============================================================================
-umount_all "${INSTALLDIR}/" || true
+umount_all "${INSTALL_DIR}/" || true
